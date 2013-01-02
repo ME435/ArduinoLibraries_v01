29f4fa5fa6275c1c83fe34e0f53b1d5a99a014dd
Dave Fisher <fisherds@rose-hulman.edu>
2013-01-02 00:05:32 -0500

    First functional test with Android
    Able to send and receive communication with a demo sketch using this
    library.
    
    TODO: Add that demo as an example.

--- CommandParser/CommandParser.cpp
+++ CommandParser/CommandParser.cpp
@@ -16,7 +16,6 @@ void CommandParser::parseCommand(String command) {
 	String angleStr;
 	int angleInt, jointNumber = 1;
 	if (command.startsWith("POSITION")) {
-		Serial.println("Received a POSITION command.");
 		angleStr = command;
 		while (spaceIndex != -1 && jointNumber < 8) {
 			angleStr = angleStr.substring(spaceIndex + 1);
@@ -26,19 +25,16 @@ void CommandParser::parseCommand(String command) {
 			spaceIndex = angleStr.indexOf(' ');
 		}
 	} else if (command.startsWith("GRIPPER")) {
-		Serial.println("Received a GRIPPER command.");
 		String gripperValueStr = command.substring(spaceIndex + 1);
 		int gripperValueInt = gripperValueStr.toInt();
 		_robotArm.setGripperDistance(gripperValueInt);
 	} else if (command.startsWith("JOINT")) {
-		Serial.println("Received a JOINT command.");
 		String jointNumStr = command.substring(spaceIndex + 1, spaceIndex + 2);
 		jointNumber = jointNumStr.toInt();
-		// For now I'm ignoring the next keyword
+		// For now I'm ignoring the next keyword ANGLE
 		int lastSpaceIndex = command.lastIndexOf(' ');
 		angleStr = command.substring(lastSpaceIndex + 1);
 		angleInt = angleStr.toInt();
 		_robotArm.setJointAngle(jointNumber, angleInt);
 	}
-
 }
