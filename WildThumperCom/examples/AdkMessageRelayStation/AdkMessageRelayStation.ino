add66f1c12e06b3894b90cc70deb98358cac9d44
Dave Fisher <fisherds@rose-hulman.edu>
2013-04-20 21:35:01 -0400

    Library reorg
    Moved the TimerEvent class into the TimerEventScheduler library folder.
    Removed all ArmScripts_ArmOnThumper examples.  Moved
    AdkMessageRelayStation to WildThumperCom.

--- WildThumperCom/examples/AdkMessageRelayStation/AdkMessageRelayStation.ino
+++ WildThumperCom/examples/AdkMessageRelayStation/AdkMessageRelayStation.ino
@@ -5,27 +5,37 @@
 #include <RobotAsciiCom.h>
 #include <WildThumperCom.h>
 
-#define TEAM_NUMBER 48
+#define TEAM_NUMBER 4  // Replace this with your team number.
+
 char manufacturer[] = "Rose-Hulman";
-char model[] = "Arm Scripts";
+char model[] = "Arm Scripts";  // Change to your app name.
 char versionStr[] = "1.0";
         
 // Only Manufacturer, Model, and Version matter to Android
 AndroidAccessory acc(manufacturer,
                      model,
-                     "Basic command set to carry out Arm Scripts",
+                     "ME435 robot arm message relay station.",
                      versionStr,
                      "https://sites.google.com/site/me435spring2013/",
                      "12345");
+
 byte rxBuf[255];
-#define BATTERY_VOLTAGE_REPLY_LENGTH 27
-byte txBuf[64];
-// Note, when sending commands to Android I don't add the '\n'.
-// Turned out to be easier to just assume the whole message arrives together.
-// Seems to work fine.  Just separate into different write() commands.
-char rightButtonScript[] = "port_ball";
-char leftButtonScript[] = "center_ball";
-char selectButtonScript[] = "starboard_ball";
+char txBuf[64];
+int batteryVoltageReplyLength = 0;
+int wheelCurrentReplyLength = 0;
+// Note, when sending commands <b>to</b> Android I don't add the '\n'.
+// Turned out to be easier since the whole message arrives together.
+
+// Just a random set of scripts that you might make for testing.
+char placeBallOn1Script[] = "place_ball_on_1";
+char placeBallOn2Script[] = "place_ball_on_2";
+char placeBallOn3Script[] = "place_ball_on_3";
+char black1Script[] = "black_1";
+char nonblack1Script[] = "nonblack_1";
+char black2Script[] = "black_2";
+char nonblack2Script[] = "nonblack_2";
+char black3Script[] = "black_3";
+char nonblack3Script[] = "nonblack_3";
 
 /***  Pin I/O   ***/ 
 #define PIN_RIGHT_BUTTON 2
@@ -34,10 +44,11 @@ char selectButtonScript[] = "starboard_ball";
 
 /*** Interrupt flags ***/
 volatile int mainEventFlags = 0;
-#define FLAG_INTERRUPT_0 0x01
-#define FLAG_INTERRUPT_1 0x02
-#define FLAG_INTERRUPT_2 0x04
-#define FLAG_NEED_TO_SEND_BATTERY_VOLTAGE 0x08
+#define FLAG_INTERRUPT_0                   0x0001
+#define FLAG_INTERRUPT_1                   0x0002
+#define FLAG_INTERRUPT_2                   0x0004
+#define FLAG_NEED_TO_SEND_BATTERY_VOLTAGE  0x0008
+#define FLAG_NEED_TO_SEND_WHEEL_CURRENT    0x0010
 
 LiquidCrystal lcd(14, 15, 16, 17, 18, 19, 20);
 #define LINE_1 0
@@ -47,7 +58,7 @@ RobotAsciiCom robotAsciiCom;
 WildThumperCom wildThumperCom(TEAM_NUMBER);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);  // Change if you are using a different baudrate.
   pinMode(PIN_LEFT_BUTTON, INPUT_PULLUP);
   pinMode(PIN_RIGHT_BUTTON, INPUT_PULLUP);
   pinMode(PIN_SELECT_BUTTON, INPUT_PULLUP);
@@ -62,13 +73,14 @@ void setup() {
   robotAsciiCom.registerJointAngleCallback(jointAngleMessageFromAndroid);
   robotAsciiCom.registerGripperCallback(gripperMessageFromAndroid);
   robotAsciiCom.registerBatteryVoltageRequestCallback(batteryVoltageRequestFromAndroid);
-  // Note, not using the wheel current feature.
+  robotAsciiCom.registerWheelCurrentRequestCallback(wheelCurrentRequestFromAndroid);
   
   // Register callbacks for commands you might receive from the Wild Thumper.
   wildThumperCom.registerBatteryVoltageReplyCallback(batteryVoltageReplyFromThumper);
+  wildThumperCom.registerWheelCurrentReplyCallback(wheelCurrentReplyFromThumper);
   
   lcd.clear();
-  lcd.print("Ready");
+  lcd.print("Relay Station");
   delay(1500);
   acc.powerOn();
 }
@@ -116,9 +128,7 @@ void jointAngleMessageFromAndroid(byte jointNumber, int jointAngle) {
 }
 
 void gripperMessageFromAndroid(int gripperDistance) {
-  if (gripperDistance < 10) {
-    gripperDistance = 10;
-  }
+  gripperDistance = constrain(gripperDistance, 10, 65);
   wildThumperCom.sendGripperDistance(gripperDistance);
   lcd.clear();
   lcd.print("Gripper:");
@@ -131,11 +141,15 @@ void batteryVoltageRequestFromAndroid(void) {
   wildThumperCom.sendBatteryVoltageRequest();
 }
 
+void wheelCurrentRequestFromAndroid(void) {
+  wildThumperCom.sendWheelCurrentRequest();
+}
+
 void batteryVoltageReplyFromThumper(int batteryMillivolts) {
   // Send to Android from within the main event loop.
   mainEventFlags |= FLAG_NEED_TO_SEND_BATTERY_VOLTAGE;
-  sprintf((char *) txBuf, "BATTERY VOLTAGE REPLY %d.%03d", batteryMillivolts / 1000, batteryMillivolts % 1000);  
-  
+  batteryVoltageReplyLength = robotAsciiCom.prepareBatteryVoltageReply(
+      batteryMillivolts, txBuf, sizeof(txBuf));
   // Display battery voltage on LCD.
   lcd.clear();
   lcd.print("Battery voltage:");
@@ -151,6 +165,37 @@ void batteryVoltageReplyFromThumper(int batteryMillivolts) {
   lcd.print(batteryMillivolts % 1000);
 }
 
+void wheelCurrentReplyFromThumper(int leftWheelMotorsMilliamps, int rightWheelMotorsMilliamps) {
+  // Send to Android from within the main event loop.
+  mainEventFlags |= FLAG_NEED_TO_SEND_WHEEL_CURRENT;
+  wheelCurrentReplyLength = robotAsciiCom.prepareWheelCurrentReply(
+      leftWheelMotorsMilliamps, rightWheelMotorsMilliamps, txBuf, sizeof(txBuf));
+
+  // Display wheel currents on LCD.
+  lcd.clear();
+  lcd.print("Wheel current:");
+  lcd.setCursor(0, LINE_2);
+  lcd.print(leftWheelMotorsMilliamps / 1000);
+  lcd.print(".");
+  if (leftWheelMotorsMilliamps % 1000  < 100) {
+    lcd.print("0");
+  }
+  if (leftWheelMotorsMilliamps % 1000 < 10) {
+    lcd.print("0");
+  }
+  lcd.print(leftWheelMotorsMilliamps % 1000);
+  lcd.print("  ");
+  lcd.print(rightWheelMotorsMilliamps / 1000);
+  lcd.print(".");
+  if (rightWheelMotorsMilliamps % 1000  < 100) {
+    lcd.print("0");
+  }
+  if (rightWheelMotorsMilliamps % 1000 < 10) {
+    lcd.print("0");
+  }
+  lcd.print(rightWheelMotorsMilliamps % 1000);
+}
+
 void loop() {
   // See if there is a new message from Android.
   if (acc.isConnected()) {
@@ -158,30 +203,36 @@ void loop() {
     if (len > 0) {
       robotAsciiCom.handleRxBytes(rxBuf, len);
     }
+    // Testing sending some scripts to Android on button press instead of a real sensor.
     if (mainEventFlags & FLAG_INTERRUPT_0) {
       delay(20);
       mainEventFlags &= ~FLAG_INTERRUPT_0;
       if (!digitalRead(PIN_RIGHT_BUTTON)) {
-        acc.write(rightButtonScript, sizeof(rightButtonScript));
+        acc.write(black2Script, sizeof(black2Script));
       }
     }
     if (mainEventFlags & FLAG_INTERRUPT_1) {
       delay(20);
       mainEventFlags &= ~FLAG_INTERRUPT_1;
       if (!digitalRead(PIN_LEFT_BUTTON)) {
-        acc.write(leftButtonScript, sizeof(leftButtonScript));
+        acc.write(nonblack2Script, sizeof(nonblack2Script));
       }
     }
     if (mainEventFlags & FLAG_INTERRUPT_2) {
       delay(20);
       mainEventFlags &= ~FLAG_INTERRUPT_2;
       if (!digitalRead(PIN_SELECT_BUTTON)) {
-        acc.write(selectButtonScript, sizeof(selectButtonScript));
+        acc.write(placeBallOn2Script, sizeof(placeBallOn2Script));
       }
     }
+    // Passing commands from the Wild Thumper on up to Android.
     if (mainEventFlags & FLAG_NEED_TO_SEND_BATTERY_VOLTAGE) {
       mainEventFlags &= ~FLAG_NEED_TO_SEND_BATTERY_VOLTAGE;
-      acc.write(txBuf, BATTERY_VOLTAGE_REPLY_LENGTH);
+      acc.write(txBuf, batteryVoltageReplyLength);
+    }
+    if (mainEventFlags & FLAG_NEED_TO_SEND_WHEEL_CURRENT) {
+      mainEventFlags &= ~FLAG_NEED_TO_SEND_WHEEL_CURRENT;
+      acc.write(txBuf, wheelCurrentReplyLength);
     }
   }
   
